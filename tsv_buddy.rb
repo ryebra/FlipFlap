046e38108ab1f27e69837dc60ca69e5c5c862b66
ryebra <ryebra@gmail.com>
2016-10-02 22:38:58 +0800

    first commit

---
+++ tsv_buddy.rb
@@ -0,0 +1,16 @@
+# Module that can be included (mixin) to take and output TSV data
+module TsvBuddy
+  # take_tsv: String w/ TSV to @data; parameter: tsv - a String in TSV format
+  def take_tsv(tsv)
+    lines = tsv.split("\n")
+    titles = lines[0].split("\t")
+    @data = (lines[1..-1]).map { |val| Hash[titles.zip(val.split("\t"))] }
+  end
+
+  # to_tsv: converts @data to tsv string; returns: String in TSV format
+  def to_tsv
+    (tsv = '') << @data[0].keys.join("\t") + "\n"
+    @data.each { |dt| tsv << dt.values.join("\t") + "\n" }
+    tsv
+  end
+end
